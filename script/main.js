ebb1b9b1ebfdcb1a427ee527d6c94b6adb500bf6
João <83858687+JoaoPedroPontes@users.noreply.github.com>
2023-01-02 17:00:22 -0300

    update

--- script/main.js
+++ script/main.js
@@ -8,22 +8,22 @@
     //pescando o elemento e atribuindo a ele um evento, nessa caso de clique e oque acontecera quando o clique acontecer.
 
     //ADICAO
-    document.getElementsByTagName("button")[0].addEventListener("click", function(events){
+    document.getElementsByTagName("button")[0].addEventListener("click", () =>{
         document.getElementsByTagName("span")[0].innerHTML="O resultado é: "+(Number(v1.value)+Number(v2.value));
     });
 
     //SUBTRACAO
-    document.getElementsByTagName("button")[1].addEventListener("click", function(events){
+    document.getElementsByTagName("button")[1].addEventListener("click", () => {
         document.getElementsByTagName("span")[0].innerHTML="O resultado é: "+(Number(v1.value)-Number(v2.value));
     });
 
     //MULTIPLICACAO
-    document.getElementsByTagName("button")[2].addEventListener("click", function(events){
+    document.getElementsByTagName("button")[2].addEventListener("click", () => {
         document.getElementsByTagName("span")[0].innerHTML="O resultado é: "+(Number(v1.value)*Number(v2.value));
     });
 
     //DIVISAO
-    document.getElementsByTagName("button")[3].addEventListener("click", function(events){
+    document.getElementsByTagName("button")[3].addEventListener("click", () => {
         document.getElementsByTagName("span")[0].innerHTML="O resultado é "+(Number(v1.value)/(Number(v2.value)))
     });
 
