bb67aacc80fe8aa50bd013796bd9fab0bc0f1da3
João <83858687+JoaoPedroPontes@users.noreply.github.com>
2023-01-02 16:58:09 -0300

    Atualizando
    mudei a estrutura como os arquivos estavam

---
+++ script/main.js
@@ -0,0 +1,34 @@
+  // funcao para adicionar os elementos, pois eles ainda nao foram carregados
+  function addEvents(){
+    //pescando os elementos
+    var v1=document.getElementById("v1");
+    var v2=document.getElementById("v2");
+    //-----------------------------------
+
+    //pescando o elemento e atribuindo a ele um evento, nessa caso de clique e oque acontecera quando o clique acontecer.
+
+    //ADICAO
+    document.getElementsByTagName("button")[0].addEventListener("click", function(events){
+        document.getElementsByTagName("span")[0].innerHTML="O resultado é: "+(Number(v1.value)+Number(v2.value));
+    });
+
+    //SUBTRACAO
+    document.getElementsByTagName("button")[1].addEventListener("click", function(events){
+        document.getElementsByTagName("span")[0].innerHTML="O resultado é: "+(Number(v1.value)-Number(v2.value));
+    });
+
+    //MULTIPLICACAO
+    document.getElementsByTagName("button")[2].addEventListener("click", function(events){
+        document.getElementsByTagName("span")[0].innerHTML="O resultado é: "+(Number(v1.value)*Number(v2.value));
+    });
+
+    //DIVISAO
+    document.getElementsByTagName("button")[3].addEventListener("click", function(events){
+        document.getElementsByTagName("span")[0].innerHTML="O resultado é "+(Number(v1.value)/(Number(v2.value)))
+    });
+
+    //-----------------------------------------------------------------------------------------------------------
+
+}
+//assim que a pagina carregar ele adicionara os elemetnos
+window.addEventListener("load", addEvents);
